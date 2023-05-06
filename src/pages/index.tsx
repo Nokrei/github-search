1736599078f0ad98fd7752e9e331400358e13347
Nok <dive877@gmail.com>
2023-05-06 17:47:47 +0400

    Create basic home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import { AiFillGithub } from "react-icons/ai";
 import Layout from "@/components/Layout";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const [selectValue, setSelectValue] = useState("");
   return (
     <Layout>
-      <p>Hello world</p>
+      <div className="flex justify-center">
+        <div>
+          <div className="flex">
+            <div className="flex w-14 items-center">
+              <AiFillGithub size={40} />
+            </div>
+            <div>
+              <h2 className="text-xl font-semibold">Github Searchbar</h2>
+              <p className="text-gray-500">
+                Search users or repositories below
+              </p>
+            </div>
+          </div>
+          <div className="flex">
+            <input
+              type="seaarch"
+              className="w-full border"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <select
+              name="options"
+              className="border"
+              value={selectValue}
+              onChange={(e) => setSelectValue(e.target.value)}
+            >
+              <option value="users">Users</option>
+              <option value="repositories">Repositories</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
